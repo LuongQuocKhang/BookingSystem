9e64e4e7cfc88c7472419cf960110109d269c108
O2F-IT\luong.quockhang_amar <luong.quockhang@amaris.com>
2023-12-26 11:47:39 +0700

    update

--- src/Services/Stay/BookingSystem.Stay.Application/Features/Queries/Amenity/GetAmenityDetail/GetAmenityQueryHandler.cs
+++ src/Services/Stay/BookingSystem.Stay.Application/Features/Queries/Amenity/GetAmenityDetail/GetAmenityQueryHandler.cs
@@ -7,19 +7,19 @@ using MediatR;
 namespace BookingSystem.Stay.Application.Features.Queries.Amenity.GetAmenities;
 
 public class GetAmenityQueryHandler(IAmenityRepository amenityRepository, IMapper mapper)
-    : IRequestHandler<GetAmenityQuery, IReadOnlyCollection<AmenityViewModel>>
+    : IRequestHandler<GetAmenityQuery, AmenityViewModel>
 {
     private readonly IAmenityRepository _amenityRepository = amenityRepository ?? throw new ArgumentNullException(nameof(amenityRepository));
 
     private readonly IMapper _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
 
-    public async Task<IReadOnlyCollection<AmenityViewModel>> Handle(GetAmenityQuery request, CancellationToken cancellationToken)
+    public async Task<AmenityViewModel> Handle(GetAmenityQuery request, CancellationToken cancellationToken)
     {
         IReadOnlyCollection<AmenityEntity> amenities = await _amenityRepository.GetAmenities()
             .ConfigureAwait(false);
 
         IReadOnlyCollection<AmenityViewModel> amenityViews = _mapper.Map<IReadOnlyCollection<AmenityViewModel>>(amenities);
 
-        return amenityViews;
+        return null;
     }
 }
